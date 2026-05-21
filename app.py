ef3257e8bf3840d797f07df478b7e77c4f2d316c
Gosukula Shirisha <gosukulashirisha02@gmail.com>
2026-05-21 11:39:47 +0530

    fixed session state

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st 
 from db_c import conn_obj,cursor_obj
 
+if "user" not in st.session_state:
+    st.session_state.user = None
+
 login, signup=st.tabs(
     ["login","signup"]
 )
@@ -10,8 +13,7 @@ dbs=cursor_obj.fetchall()
 for db in dbs:
     st.write(db)
     
-if "user" not in st.session_state:
-    st.session_state.user = None
+
 
 def login_function():
     st.header("Login")
